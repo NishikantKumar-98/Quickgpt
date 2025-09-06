8c6f90e61fe8abb8e101c9ad7e659c7c4e4eb478
unknown <nishikantkumar9871@gmail.com>
2025-09-06 14:40:53 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import Sidebar from './components/Sidebar'
+import { Route, Routes } from 'react-router-dom'
+import ChatBox from './components/ChatBox'
+import Credits from './pages/Credits'
+import Community from './pages/Community'
+import { assets } from './assets/assets'
+
+const App = () => {
+  const [isMenuOPen, setIsMenuOpen] = useState(false)
+  return (
+    <>
+       {!isMenuOPen && <img src={assets.menu_icon} className='absolute top-3 left-3 w-8 h-8 cursor-pointer md:hidden not-dark:invert' onClick={()=>setIsMenuOpen(true)}/>}
+    <div className='dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white'>
+
+    <div className='flex h-screen w-screen'>
+      <Sidebar isMenuOPen={isMenuOPen} setIsMenuOpen={setIsMenuOpen} />
+      <Routes>
+        <Route path='/' element={<ChatBox/>}/>
+        <Route path='/credits' element={<Credits/>}/>
+        <Route path='/community' element={<Community/>}/>
+      </Routes>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default App
